6c5de3d38dabd824fbae78463efc73cc98c6dca7
Alexander Ungar <alexander.ungar@tum.de>
2023-03-22 00:56:59 +0100

    Improve build pipeline

--- src/repository/budget/mongo/budgetMongoRepository.ts
+++ src/repository/budget/mongo/budgetMongoRepository.ts
@@ -1,9 +1,9 @@
 import type BudgetRepository from "../budgetRepository.js";
 import { BudgetEntityConverter } from "../entity/converters.js";
-import { BudgetModel } from "./models.js";
+import { BudgetSummaryModel } from "./models.js";
 
 export const findBudgets: BudgetRepository["findAll"] = async () => {
-  const found = await BudgetModel.find().select("-transactions");
+  const found = await BudgetSummaryModel.find().select("-transactions");
   return found.map(BudgetEntityConverter.toDomain);
 };
 
