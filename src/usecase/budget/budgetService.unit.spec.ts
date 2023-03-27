7b8392c36d6cd76298444bd84eee1b69406d9b9b
Alexander Ungar <alexander.ungar@tum.de>
2023-03-27 14:35:01 +0200

    Fix budgetService.unit.spec.ts compile errors

--- src/usecase/budget/budgetService.unit.spec.ts
+++ src/usecase/budget/budgetService.unit.spec.ts
@@ -1,16 +1,26 @@
-import { createBudget } from "./budgetService.js";
 import type BudgetUseCases from "./budgetUseCases.js";
-import type BudgetSummaryRepository from "../../repository/budget/budgetSummaryRepository.js";
-import { BudgetSummary, type NewBudget } from "../../domain/budget.js";
+import { type BudgetSummary, type NewBudget } from "../../domain/budget.js";
 import Limit from "../../domain/limit.js";
-import BudgetService from "./budgetService.js";
+import { createBudget } from "./budgetService.js";
 
 describe("budgetService", () => {
-  
   beforeAll(() => {});
   it("calls the create function with the correct object", async () => {
     const newBudget: NewBudget = { name: "My Budget", limit: new Limit(250) };
-    const expectedBudgetSummary: BudgetSummary = {...newBudget, spent: 0, transactions: [], id: ''}
-    (createBudget: BudgetUseCases["createBudget"]) => (newBudget) =>
+    const expectedBudgetSummary: Omit<BudgetSummary, "id"> = {
+      ...newBudget,
+      spent: 0,
+      transactions: [],
+    };
+    const insertBudgetMock = jest.fn();
+    const testSubject: BudgetUseCases["createBudget"] =
+      createBudget(insertBudgetMock);
+    insertBudgetMock.mockImplementation((budgetSummary) => budgetSummary);
+
+    const insertedBudgetSummary = await testSubject(newBudget);
+
+    expect(insertedBudgetSummary).toEqual(
+      expect.objectContaining(expectedBudgetSummary)
+    );
   });
 });
