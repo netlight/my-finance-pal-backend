a35bff4e5ac2377b76c13ca881185c78da19873c
Arda Özdere <arda.oezdere@ext.elli.eco>
2023-04-03 22:15:07 +0200

    Empty version of the unit test.

---
+++ src/repository/budget/entity/converters.unit.spec.ts
@@ -0,0 +1,16 @@
+import { type Budget } from "../../../domain/budget";
+import Limit from "../../../domain/limit";
+import UUID from "../../../domain/uuid";
+import type BudgetEntity from "./budgetEntity";
+import { BudgetEntityConverter } from "./converters";
+
+const testId = "2d223edd-f5a0-472b-945f-c0e0d60476f0";
+
+describe("converters", () => {
+  beforeAll(() => {});
+  it("converts budget entity successfully", async () => {
+    // GIVEN
+    // WHEN
+    // THEN
+  });
+});
