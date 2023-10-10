3f1afda774df685baa445150adb2f7023438f4c4
Alexander Ungar <alexander.ungar@tum.de>
2023-10-10 20:32:19 +0200

    Add solution for exercise 1 and TODOs for #2

--- src/repository/budget/entity/budgetSummaryEntity.ts
+++ src/repository/budget/entity/budgetSummaryEntity.ts
@@ -1,5 +1,12 @@
 import type ExpenseEntity from "../../expense/entity/expenseEntity";
 
+/*
+TODO 2.1 - have a look at the entity.
+  Why might this be a good/bad idea when modeling the data for MongoDB?
+  What would you change for an SQL db?
+  What other approach could we have chosen for modeling for NoSQL?
+*/
+
 interface BudgetSummaryEntity {
   id: string;
   name: string;
